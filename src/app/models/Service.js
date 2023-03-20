7d706ff0ae26f4cdacdc2135122e208247eac066
thang <thangvhv99@gmail.com>
2023-03-20 22:19:47 +0700

    update

--- src/app/models/Service.js
+++ src/app/models/Service.js
@@ -10,6 +10,7 @@ const Service = new Schema({
     price: {type: Number, required: true},
     detail: {type: String, required: true},
     status: {type: String, required: true, default: 'Đang hoạt động'},
+    imgURLs: {type: Array},
     room_id: {type: mongoose.Schema.Types.ObjectId, ref: "room"},
     serviceMonth_id: [{type: mongoose.Schema.Types.ObjectId, ref: "servicemonth"}],
 }, {
